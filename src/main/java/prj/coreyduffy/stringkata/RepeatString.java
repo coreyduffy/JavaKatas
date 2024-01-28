5fa89e696fc8f7e2ceda7872278e7874b1133da8
corey-duffy <coreyjduffy@hotmail.com>
2024-01-28 20:38:00 +0000

    Adding initial kata and README

---
+++ src/main/java/prj/coreyduffy/stringkata/RepeatString.java
@@ -0,0 +1,14 @@
+package prj.coreyduffy.stringkata;
+
+public class RepeatString {
+
+    // A function that accepts an integer 'repeat' and a string 's' as parameters,
+    // and returns a string of s repeated exactly 'repeat' times.
+    public static String repeatString(final int repeat, final String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < repeat; i++) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
